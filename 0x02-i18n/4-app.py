c74d38d932f4fb3fd9cc002528aaa33ff15d9caf
AJIyanu <aderemijoseph@live.com>
2023-03-06 22:05:46 +0100

    users

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -35,7 +35,7 @@ def index() -> str:
     """route to home"""
     home_title = _("home_title")
     home_header = _("home_header")
-    return render_template('3-index.html',
+    return render_template('4-index.html',
                            home_title=home_title,
                            home_header=home_header)
 
---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+"""creates a flask application"""
+
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel, gettext as _
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """to configure available languages"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user(user_id: int) -> dict:
+    """fetch user for me"""
+    return user.get(user_id)
+
+
+@app.before_request
+def before_request() -> None:
+    """gets user before handling request"""
+    if bool(request.args):
+        login_as = request.args.get('login_as')
+        user = get_user(login_as)
+        g.user = user
+
+
+@babel.localeselector
+def get_locale():
+    """set locale en"""
+    locale = request.args.get("locale")
+    lang = ["en", "fr"]
+    if locale in lang:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def index() -> str:
+    """route to home"""
+    home_title = _("home_title")
+    home_header = _("home_header")
+    username = 
+    return render_template('5-index.html',
+                           home_title=home_title,
+                           home_header=home_header
+                           username=username)
+
+
+if __name__ == "__main__":
+    host = "0.0.0.0"
+    port = "5000"
+    app.run(host=host, port=port)
