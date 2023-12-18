23ea3ae9acd582f0b9988037673d2df5b0dcac4e
Aderemi Joseph <99488361+AJIyanu@users.noreply.github.com>
2023-12-18 17:37:54 +0100

    check

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,24 @@
+import { createClient, print } from "redis";
+
+
+const client = createClient();
+
+client.on('error', (err) => {
+    console.log(`Redis client not connected to the server: ${err.toString()}`);
+})
+
+client.on('connect', () => console.log('Redis client connected to the server'));
+
+client.subscribe("holberton school channel");
+
+function killServer() {
+    client.unsubscribe('holberton school channel');
+    client.QUIT();
+}
+
+client.on('message', (channel, message) => {
+    if (message === "KILL_SERVER") {
+        killServer();
+    }
+    console.log(message);
+})
